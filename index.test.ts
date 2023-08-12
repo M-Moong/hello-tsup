f239e7280c42d769e86e6ffbefc3bff3e98bc03e
Moong <tmdals9154@gmail.com>
2023-08-12 17:05:19 +0900

    ✨ 테스트 완료?

--- index.test.ts
+++ index.test.ts
@@ -1,4 +1,3 @@
-//# 요구사항은 아래와 같은 보조 목표의 총 합이 되었습니다.
 // boolean 은 빈 값이 아니다.
 // null 은 빈 값으로 처리한다
 // undefined 는 빈 값으로 처리한다
@@ -10,13 +9,31 @@
 
 import { isEmpty } from './index'
 
-// console.log(isEmpty(true)); // false
-// console.log(isEmpty(false)); // false
-
-describe('isEmpty함수는', () => {
-	// boolean 은 빈 값이 아니다.
-	test('boolean은 빈 값이 아니다', () => {
-		expect(isEmpty(true)).toBe(false);
-		expect(isEmpty(false)).toBe(false);
-	})
+describe('isEmpty 함수는', () => {
+    test('boolean 은 빈 값이 아니다', () => {
+        expect(isEmpty(true)).toBe(false);
+        expect(isEmpty(false)).toBe(false);
+    });
+    test('null 은 빈 값으로 처리한다', () => {
+        expect(isEmpty(null)).toBe(true);
+    });
+    test('undefined 는 빈 값으로 처리한다', () => {
+        expect(isEmpty(undefined)).toBe(true);
+    });
+    test('number 는 빈 값이 아니다', () => {
+        expect(isEmpty(0)).toBe(false);
+        expect(isEmpty(1)).toBe(false);
+    });
+    test('string의 길이가 0 이면 빈 값으로 처리한다', () => {
+        expect(isEmpty('')).toBe(true);
+    });
+    test('symbol 은 빈 값이 아니다.', () => {
+        expect(isEmpty(Symbol())).toBe(false);
+    });
+    test('배열의 길이가 0이면 빈 값으로 처리한다', () => {
+        expect(isEmpty([])).toBe(true);
+    });
+    test('객체에 프로퍼티가 없으면 빈 값으로 처리한다', () => {
+        expect(isEmpty({})).toBe(true);
+    });
 })
--- index.ts
+++ index.ts
@@ -1,4 +1,3 @@
-//# 요구사항은 아래와 같은 보조 목표의 총 합이 되었습니다.
 // boolean 은 빈 값이 아니다.
 // null 은 빈 값으로 처리한다
 // undefined 는 빈 값으로 처리한다
@@ -9,10 +8,45 @@
 // 객체에 프로퍼티가 없으면 빈 값으로 처리한다.
 
 export const isEmpty = (value: any) => {
-	// boolean은 빈 값이 아니다.
-	if (typeof value === 'boolean') {
-		return false
-	}
+    // boolean 은 빈 값이 아니다.
+    if (typeof value === 'boolean') {
+        return false
+    }
 
-	return true
+    // null 은 빈 값으로 처리한다
+    if (value === null) {
+        return true
+    }
+
+    // undefined 는 빈 값으로 처리한다
+    if (value === undefined) {
+        return true
+    }
+
+    // number 는 빈 값이 아니다.
+    if (typeof value === 'number') {
+        return false;
+    }
+
+    // string의 길이가 0 이면 빈 값으로 처리한다
+    if (typeof value === 'string' && value.length === 0) {
+        return true;
+    }
+
+    // symbol 은 빈 값이 아니다.
+    if (typeof value === 'symbol') {
+        return false;
+    }
+
+    // 배열의 길이가 0이면 빈 값으로 처리한다
+    if (Array.isArray(value) && value.length === 0) {
+        return true
+    }
+    
+    // 객체에 프로퍼티가 없으면 빈 값으로 처리한다.
+    if (typeof value === 'object' && Object.keys(value).length === 0) {
+        return true;
+    }
+
+    return
 }
